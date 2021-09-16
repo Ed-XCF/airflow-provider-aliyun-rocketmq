d2060c036e636500a12acf4b50f1238dda57cbad
Ed-XCF <m.tofu@qq.com>
2021-09-16 17:44:33 +0800

    fix version

--- aliyun_rocketmq_provider/__init__.py
+++ aliyun_rocketmq_provider/__init__.py
@@ -1,11 +1,8 @@
-with open("version", "r") as fh:
-    version = fh.read()
-
 def get_provider_info():
     return {
         "package-name": "apache-airflow-providers-aliyun-rocketmq",
         "name": "Aliyun RocketMQ Airflow Provider",
         "description": "Airflow provider for aliyun rocketmq",
         "hook-class-names": ["aliyun_rocketmq_provider.hooks.aliyun_rocketmq.AliyunRocketMQHook"],
-        "versions": [version]
+        "versions": ["0.0.6"]
     }
